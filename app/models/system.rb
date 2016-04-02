cbc5a3e11d07f71d702868136f489550d7287667
-e <philipp.christen@gmail.com>
2016-04-02 16:41:11 +0200

    first unit test for models

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,3 +1,5 @@
 class System < ActiveRecord::Base
   belongs_to :system_group
+
+  validates_presence_of :urn
 end
--- test/controllers/api/v1/api_controller_test.rb
+++ test/controllers/api/v1/api_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class Api::V1::ApiControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+
+  test "should not save System without data" do
+    sys = System.new
+    assert_not sys.save
+  end
 end
