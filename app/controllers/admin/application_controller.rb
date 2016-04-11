2c1557468e5efe83ae8afa5ee5840e35082fb674
Philipp Christen <philipp.christen@gmail.com>
2016-04-11 16:51:56 +0200

    enable using will_paginate and kaminari at the same time

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -12,6 +12,21 @@ module Admin
       # TODO Add authentication logic here.
     end
 
+    # Fixes the "undefined method per" bug (caused by using will_paginate and kaminari gems simultaneously)
+    def index
+      search_term = params[:search].to_s.strip
+      resources = Administrate::Search.new(resource_resolver, search_term).run
+      resources = order.apply(resources)
+      resources = resources.paginate(:page => params[:page])     
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+
+      render locals: {
+        resources: resources.paginate(:page => params[:page]),
+        search_term: search_term,
+        page: page,
+      }
+    end
+
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
     # def records_per_page
