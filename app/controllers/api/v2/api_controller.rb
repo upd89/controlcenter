a7dc01b98a34f1ad1f0e0fef80323a53aac5fbc4
Philipp Christen <philipp.christen@gmail.com>
2016-05-05 10:18:33 +0200

    prepared check for CN header

--- app/controllers/api/v2/api_controller.rb
+++ app/controllers/api/v2/api_controller.rb
@@ -55,7 +55,10 @@ module Api::V2
     def register
       data = JSON.parse request.body.read
 
-      #if check_mandatory_json_params(data, ["name", "urn", "os", "address"]) || !request.headers['X-Api-Client-Cert'] #TODO deny if no client cert was given
+      #if check_mandatory_json_params(data, ["name", "urn", "os", "address"])
+      #   || !request.headers['X-Api-Client-Cert'] #TODO deny if no client cert was given
+      #   || !request.headers['X-Api-Client-CN']
+      #   || request.headers['X-Api-Client-CN'] != data['name']
       if check_mandatory_json_params(data, ["name", "urn", "os", "address"])
         render json: { status: "ERROR" }
         return
