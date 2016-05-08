a0ed2ce919181ba7f59995c9a12689d8b296ae58
Philipp Christen <philipp.christen@gmail.com>
2016-05-08 18:06:47 +0200

    testing should include Sorcery

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe "Users", type: :request do
+
   describe "GET /users" do
+
     it "works! (now write some real specs)" do
       get users_path
       expect(response).to have_http_status(200)
