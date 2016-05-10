fa5378ed94ae866f6bf590647e6f95414bdb24cd
Philipp Christen <philipp.christen@gmail.com>
2016-05-10 10:23:18 +0200

    return to Faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,8 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do |f|
-    f.sequence(:name) {|n| "person#{n}"}
-    #f.name Faker::Internet.user_name
-    f.sequence(:email) {|n| "foo#{n}@example.com"}
-    #f.email Faker::Internet.email
+    f.name Faker::Internet.user_name
+    f.email Faker::Internet.email
     f.role
     f.crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt("secret", "asdasdastr4325234324sdfds") }
     f.salt "asdasdastr4325234324sdfds"
