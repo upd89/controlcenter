e4bfa25672fea055ae0932640f2bd7002987c0f3
Ueli Bosshard <ubosshard@gmail.com>
2016-05-18 17:28:04 +0200

    modified controller for filtered view

--- app/controllers/system_package_relations_controller.rb
+++ app/controllers/system_package_relations_controller.rb
@@ -1,6 +1,45 @@
 class SystemPackageRelationsController < ApplicationController
+
+  class Pkg
+    attr_reader :pkg_id
+    attr_reader :pkg_name
+    attr_reader :pkg_section
+    attr_reader :count
+
+    def initialize(id, name, section)
+      @pkg_id = id
+      @pkg_name = name
+      @pkg_section = section
+      @count = 1
+    end
+
+    def to_s
+      "#{pkg_id}, #{pkg_name}, #{pkg_name}"
+    end
+
+    def increment()
+      @count += 1
+    end
+
+  end
+
+
   # GET /systems
   def index
-    @system_package_relations = SystemPackageRelation.all
+    allSystemPackageRelations = SystemPackageRelation.all
+    @system_package_relations = {}
+    allSystemPackageRelations.each do |p|
+      if @system_package_relations[p.pkg_id]
+        @system_package_relations[p.pkg_id].increment()
+      else
+        @system_package_relations[p.pkg_id] = Pkg.new(p.pkg_id, p.pkg_name, p.pkg_section)
+      end
+    end
   end
+
+  # GET /systems/id
+  def show
+    @system_package_relations = SystemPackageRelation.where(:pkg_id => @system_package_relation)
+  end
+
 end
