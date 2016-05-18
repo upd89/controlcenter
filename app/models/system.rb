252495a6e067f461f4c6e17f2be23d7eed94f8f2
Philipp Christen <philipp.christen@gmail.com>
2016-05-18 11:40:14 +0200

    show tasks in System Detail WIP

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,10 +1,12 @@
 # a single server node where an agent is installed
 class System < ActiveRecord::Base
   belongs_to :system_group
-  has_many :concrete_package_versions
 
+  has_many :concrete_package_versions
   has_many :package_versions, -> { distinct }, through: :concrete_package_versions
   has_many :packages, -> { distinct }, through: :package_versions
+
+  has_many :tasks, -> { distinct }, through: :concrete_package_versions
 
   validates_presence_of :urn
 
