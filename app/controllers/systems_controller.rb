5abea0fb2202561891a3d445be7b4daa97ac7bbe
Philipp Christen <philipp.christen@gmail.com>
2016-05-18 16:51:32 +0200

    extended system view WIP

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -27,6 +27,15 @@ class SystemsController < ApplicationController
     ) or return
     @concrete_package_versions = @filterrific_show.find.page(params[:page])
     @installableCPVs = ConcretePackageVersion.where(system: @system, concrete_package_state: ConcretePackageState.where(name: "Available")[0] )
+
+    @filterrific_show_tasks = initialize_filterrific(
+      Task,
+      params[:filterrific_task],
+      :select_options => {
+        with_task_state_id: TaskState.options_for_select
+      }
+    ) or return
+    @tasks = @filterrific_show_tasks.find.page(params[:page])
   end
 
   # POST /systems
--- app/models/task_state.rb
+++ app/models/task_state.rb
@@ -1,2 +1,7 @@
 class TaskState < ActiveRecord::Base
+  validates_presence_of :name
+  
+  def self.options_for_select
+    order('LOWER(name)').map { |e| [e.name, e.id] }
+  end
 end
