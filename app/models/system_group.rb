aa99374f444cf0e127d6e700b8a8b66a52ec6d4e
Philipp Christen <philipp.christen@gmail.com>
2016-05-25 16:33:50 +0200

    show updatable system groups in dashboard

--- app/models/system_group.rb
+++ app/models/system_group.rb
@@ -6,4 +6,12 @@ class SystemGroup < ActiveRecord::Base
   def self.options_for_select
     order('LOWER(name)').map { |e| [e.name, e.id] }
   end
+
+  def has_installable_CPVs
+    systems.reject{ |s| s.get_installable_CPVs.count < 1 }.count > 0
+  end
+
+  def get_nr_of_installable_CPVs
+    systems.map{ |s| s.get_installable_CPVs.count }.inject(:+)
+  end
 end
