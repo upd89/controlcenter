9332bb14184619b6e3660436558b29ce52bc2aea
Ueli Bosshard <ubosshard@gmail.com>
2016-05-25 17:10:05 +0200

    extracted method updateSystemHash to service

--- app/models/data_mutation_service.rb
+++ app/models/data_mutation_service.rb
@@ -2,8 +2,38 @@
 class DataMutationService 
 
     def self.register(data)
-        system = System.get_maybe_create(data)
+        System.get_maybe_create(data)
         return { status: "OK" }
+    end
+
+    def self.updateSystemHash(urn, data)
+      currentSys = System.where(urn: urn)[0]
+      currentSys.update_last_seen()
+      currentSys.apply_properties(data)
+      currentSys.save()
+
+      unknownPackages = []
+      knownPackages = []
+      stateAvailable = ConcretePackageState.first
+
+      data["packageUpdates"].each do |updHash|
+        if PackageVersion.exists?( sha256: updHash )
+          knownPackages.push( updHash )
+          pkgVersion = PackageVersion.where( sha256: updHash )[0]
+          ConcretePackageVersion.create_new(pkgVersion, currentSys, stateAvailable)
+        else
+          unknownPackages.push( updHash )
+        end
+      end
+      currentSys.save()
+
+      if unknownPackages.length > 0
+        return { status: "infoIncomplete", knownPackages: knownPackages }
+      elsif currentSys.concrete_package_versions.where(concrete_package_state: ConcretePackageState.where(name: "Available")[0]).count != data["updCount"]
+        return { status: "countMismatch", knownPackages: knownPackages }
+      else
+        return { status: "OK", knownPackages: knownPackages  }
+      end
     end
 
 end
