0138c7c8e0395f24927dc3019aab2b620c9a5611
Philipp Christen <philipp.christen@gmail.com>
2016-06-01 13:13:01 +0200

    removed spinning gif

--- app/controllers/concrete_package_versions_controller.rb
+++ app/controllers/concrete_package_versions_controller.rb
@@ -14,52 +14,32 @@ class ConcretePackageVersionsController < ApplicationController
       }
     ) or return
     @concrete_package_versions = @filterrific.find.page(params[:page])
-    #@concrete_package_versions = ConcretePackageVersion.all
-  end
-
-  # GET /concrete_package_versions/new
-  def new
-    @concrete_package_version = ConcretePackageVersion.new
   end
 
   # POST /concrete_package_versions
-  # POST /concrete_package_versions.json
   def create
     @concrete_package_version = ConcretePackageVersion.new(concrete_package_version_params)
 
-    respond_to do |format|
-      if @concrete_package_version.save
-        format.html { redirect_to @concrete_package_version, success: 'Concrete package version was successfully created.' }
-        format.json { render :show, status: :created, location: @concrete_package_version }
-      else
-        format.html { render :new }
-        format.json { render json: @concrete_package_version.errors, status: :unprocessable_entity }
-      end
+    if @concrete_package_version.save
+      redirect_to @concrete_package_version, success: 'Concrete package version was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /concrete_package_versions/1
-  # PATCH/PUT /concrete_package_versions/1.json
   def update
-    respond_to do |format|
-      if @concrete_package_version.update(concrete_package_version_params)
-        format.html { redirect_to @concrete_package_version, success: 'Concrete package version was successfully updated.' }
-        format.json { render :show, status: :ok, location: @concrete_package_version }
-      else
-        format.html { render :edit }
-        format.json { render json: @concrete_package_version.errors, status: :unprocessable_entity }
-      end
+    if @concrete_package_version.update(concrete_package_version_params)
+      redirect_to @concrete_package_version, success: 'Concrete package version was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /concrete_package_versions/1
-  # DELETE /concrete_package_versions/1.json
   def destroy
     @concrete_package_version.destroy
-    respond_to do |format|
-      format.html { redirect_to concrete_package_versions_url, success: 'Concrete package version was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to concrete_package_versions_url, success: 'Concrete package version was successfully destroyed.'
   end
 
   private
