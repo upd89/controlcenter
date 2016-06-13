999f9b5a77e03c56d418cafacf29dcef2617c913
Philipp Christen <philipp.christen@gmail.com>
2016-06-13 07:27:52 +0200

    package unit tests

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Package, type: :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:package)).to be_valid
   end
+
+  it "has a permission_level of its group" do
+    # grp = FactoryGirl.create(:package_group, permission_level: 5)
+    grp = PackageGroup.create(name: "Test", permission_level: 5)
+    pkg = FactoryGirl.create(:package)
+    GroupAssignment.create( package: pkg, package_group: grp)
+
+    expect(pkg.get_permission_level).to be 5
+  end
+
+  it "has a permission_level even if not in a group" do
+    expect(FactoryGirl.create(:package).get_permission_level).to be 0
+  end
 end
